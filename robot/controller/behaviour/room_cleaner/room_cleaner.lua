0f94c7ad9af406dc44a3b79a6f33557161e6c616
Francesco Grandinetti <francesco.grandinett@gmail.com>
2021-05-23 20:04:31 +0200

    handle navigation to dirty cell in RoomCleaner

--- robot/controller/behaviour/room_cleaner/room_cleaner.lua
+++ robot/controller/behaviour/room_cleaner/room_cleaner.lua
@@ -2,6 +2,7 @@ local Color = require('util.commons').Color
 local Position = require('util.commons').Position
 local Direction = require('util.commons').Direction
 local logger = require('util.logger')
+local LogLevel = logger.LogLevel
 local Set = require('util.set')
 
 local RobotAction = require('robot.commons').Action
@@ -24,13 +25,15 @@ local function isRobotNearLastKnownPosition(oldPosition, newPosition)
         or oldPosition.lng == newPosition.lng and oldPosition.lat == newPosition.lat + 1
 end
 
-local function getFirstDirtyCell(map)
+local function getFirstDirtyCell(map, dirtPositionsToSkip)
     local length = #map
+    dirtPositionsToSkip = dirtPositionsToSkip or Set:new{}
     for i = 0, length do
         local rowLength = #map[i]
         for j = 0, rowLength do
-            if map[i][j] == CellStatus.DIRTY then
-                return Position:new(i, j)
+            local position = Position:new(i, j)
+            if map:getCell(position) == CellStatus.DIRTY and not dirtPositionsToSkip:contain(position) then
+                return position
             end
         end
     end
@@ -92,6 +95,7 @@ RoomCleaner = {
             map = map,
             moveExecutioner = MoveExecutioner:new(map),
             planner = nil,
+            target = nil,
             lastKnownPosition = map.position,
             oldDirection = Direction.NORTH
         }
@@ -102,59 +106,84 @@ RoomCleaner = {
 
     tick = function (self, state)
         if state.isDirtDetected then
-            local dirtPositions = detectDirtyPositions(
-                state,
-                self.map,
-                self.lastKnownPosition,
-                self.oldDirection
-            )
-            for i = 1, #dirtPositions do
-                self.planner:setCellAsDirty(dirtPositions[i])
-                self.map:setCellAsDirty(dirtPositions[i])
-            end
-            return RobotAction:new({
-                hasToClean = true,
-                leds = { switchedOn = true, color = Color.WHITE },
-                speed = { left = 0, right = 0}
-            }, {1, 3})
-        elseif isRobotNearLastKnownPosition(self.lastKnownPosition, self.map.position) then
-            if self.state == State.WORKING then
-                return self:working(state)
-            else
-                logger.print('[ROOM_CLEANER] Unhandled state', logger.LogLevel.WARNING)
-            end
-        else
-            local dirtPosition = getFirstDirtyCell(self.map)
-            if dirtPosition ~= nil then
-                self.planner = Planner:new(self.map)
-                self.planner:addNewDiagonalPoint(#self.map.map)
-                while dirtPosition ~= nil do
-                    local success = self:computeActionsToDirtState(state, dirtPosition)
-                    if success then
-                        return RobotAction:new({})
-                    end
-                    dirtPosition = getFirstDirtyCell(self.map)
-                end
-            end
-
+            return self:handleDirtyCell(state)
+        elseif self.state == State.WORKING then
             return self:working(state)
+        elseif self.state == State.GOING_TO_DIRT then
+            return self:reachDirtPosition(state)
+        elseif self.state == State.OBSTACLE_ENCOUNTERED then
+            return self:handleObstacle(state)
+        else
+            logger.printToConsole('[ROOM_CLEANER] Unknown state: ' .. self.state, LogLevel.WARNING)
+            logger.printTo('[ROOM_CLEANER] Unknown state: ' .. self.state, LogLevel.WARNING)
         end
     end,
 
-    working = function (self)
+    working = function (self, state)
+        if not isRobotNearLastKnownPosition(self.lastKnownPosition, self.map.position) then
+            return self:handleDifferentPosition(state)
+        end
+
         if self.map:getCurrentCell() == CellStatus.DIRTY then
             self.map:setCellAsClean(self.map.position)
         end
         self.lastKnownPosition = self.map.position
+        self.target = nil
         return RobotAction:new({})
     end,
 
-    computeActionsToDirtState = function (self, state, dirtPosition)
+    handleDirtyCell = function (self, state)
+        local dirtPositions = detectDirtyPositions(
+            state,
+            self.map,
+            self.lastKnownPosition,
+            self.oldDirection
+        )
+        for i = 1, #dirtPositions do
+            self.planner:setCellAsDirty(dirtPositions[i])
+            self.map:setCellAsDirty(dirtPositions[i])
+        end
+        return RobotAction:new({
+            hasToClean = true,
+            leds = { switchedOn = true, color = Color.WHITE },
+            speed = { left = 0, right = 0}
+        }, {1, 3})
+    end,
+
+    handleDifferentPosition = function (self, state)
+        local dirtPosition = getFirstDirtyCell(self.map)
+        if dirtPosition ~= nil then
+            local currentDepth = #self.map.map
+            local dirtPositionsToSkip = Set:new{}
+            self.planner = Planner:new(self.map)
+            self.planner:addNewDiagonalPoint(currentDepth)
+            while dirtPosition ~= nil do
+                local success = self:computeActionsToDirtCell(state, dirtPosition, currentDepth)
+                if success then
+                    self.target = dirtPosition
+                    return RobotAction.stayStill({1,3})
+                else
+                    --[[
+                        if the cell can't be currently reached it is added to this set
+                        so that the while loop can end
+                    ]]
+                    dirtPositionsToSkip:add(dirtPosition)
+                end
+                dirtPosition = getFirstDirtyCell(self.map, dirtPositionsToSkip)
+            end
+        end
+
+        self.state = State.WORKING
+        return self:working(state)
+    end,
+
+    computeActionsToDirtCell = function (self, state, dirtPosition, currentDepth)
         local excludedOptions = getExcludedOptionsByState(state)
+        local currentDirection = controller_utils.discreteDirection(state.robotDirection)
         local actions = self.planner:getActionsTo(
             self.map.position,
             dirtPosition,
-            controller_utils.discreteDirection(state.robotDirection),
+            currentDirection,
             excludedOptions
         )
         self.lastKnownPosition = self.map.position
@@ -164,12 +193,12 @@ RoomCleaner = {
             self.state = State.GOING_TO_DIRT
             return true
         else
-            self.planner:addNewDiagonalPoint(#self.map.map)
-            self.map:addNewDiagonalPoint(#self.map.map)
+            self.planner:addNewDiagonalPoint(currentDepth + 1)
+            self.map:addNewDiagonalPoint(currentDepth + 1)
             actions = self.planner:getActionsTo(
                 self.map.position,
                 dirtPosition,
-                controller_utils.discreteDirection(state.robotDirection),
+                currentDirection,
                 excludedOptions
             )
             if actions ~= nil and #actions > 0 then
@@ -177,11 +206,107 @@ RoomCleaner = {
                 self.state = State.GOING_TO_DIRT
                 return true
             else
-                -- set cell as an obstacle
+                if self.map.isPerimeterIdentified then
+                    self.map:setCellAsObstacle(dirtPosition)
+                end
                 return false
             end
         end
-    end
+    end,
+
+    reachDirtPosition = function (self, state)
+
+        if self.lastKnownPosition ~= self.map.position then
+            return self:handleDifferentPosition(state)
+        end
+
+        local result = self.moveExecutioner:doNextMove(state)
+        self.lastKnownPosition = result.position
+        self.map.position = result.position
+        -- subsume no matter what the room coverage level
+        table.insert(result.action.levelsToSubsume, 3)
+
+        if result.isObstacleEncountered then
+            self.state = State.OBSTACLE_ENCOUNTERED
+
+            logger.print("[ROOM_CLEANER]")
+            logger.print("Currently in " .. self.map.position:toString(), LogLevel.INFO)
+            logger.print(result.obstaclePosition:toString() .. " detected as obstacle!", LogLevel.WARNING)
+            logger.print("----------------", LogLevel.WARNING)
+
+            self.map:setCellAsObstacle(result.obstaclePosition)
+            self.planner:setCellAsObstacle(result.obstaclePosition)
+            return RobotAction:new({}, {3})
+        elseif result.isMoveActionFinished then
+            self.map:setCellAsClean(result.position)
+            self.planner:setCellAsClean(result.position)
+            self.oldDirection = controller_utils.discreteDirection(state.robotDirection)
+            return self:reachDirtPositionNextMove(state)
+        else
+            return result.action
+        end
+    end,
+
+    reachDirtPositionNextMove = function (self, state)
+        if self.moveExecutioner:hasMoreActions() then
+            local nextMove = self.moveExecutioner.actions[1]
+            if nextMove == MoveAction.GO_AHEAD then
+                return RobotAction:new({}, {3})
+            elseif nextMove == MoveAction.GO_BACK or nextMove == MoveAction.GO_BACK_BEFORE_TURNING then
+                return RobotAction.goBack({1, 3})
+            elseif nextMove == MoveAction.TURN_LEFT then
+                return RobotAction.turnLeft({1, 3})
+            elseif nextMove == MoveAction.TURN_RIGHT then
+                return RobotAction.turnRight({1, 3})
+            end
+        else
+            return self:handleDifferentPosition(state)
+        end
+    end,
+
+    handleObstacle = function (self, state)
+
+        local result = nil
+        if self.lastKnownPosition ~= self.map.position then
+            return self:handleDifferentPosition(state)
+        else
+            result = self.moveExecutioner:getAwayFromObstacle(state)
+            self.lastKnownPosition = result.position
+            self.map.position = result.position
+        end
+
+        if result.isMoveActionFinished then
+            self.planner:addNewDiagonalPoint(self.target.lat + 1)
+            self.map:addNewDiagonalPoint(self.target.lat + 1)
+            local actions = self.planner:getActionsTo(
+                self.map.position,
+                self.target,
+                controller_utils.discreteDirection(state.robotDirection),
+                getExcludedOptionsByState(state)
+            )
+
+            if actions ~= nil and #actions > 0 then
+                self.moveExecutioner:setActions(actions)
+                self.state = State.GOING_TO_DIRT
+                return RobotAction.stayStill({1, 3})
+            elseif self.map.position == self.target then
+                return self:handleDifferentPosition(state)
+            else
+                logger.print("[ROOM_CLEANER]")
+                logger.print(
+                    self.target:toString() .. " is unreachable from "
+                    .. self.map.position:toString() .. "!",
+                    LogLevel.WARNING
+                )
+                logger.print("----------------", LogLevel.INFO)
+                return self:handleDifferentPosition(state)
+            end
+        else
+            -- subsume no matter what the room coverage level
+            table.insert(result.action.levelsToSubsume, 3)
+            return result.action
+        end
+    end,
 
 }
 
